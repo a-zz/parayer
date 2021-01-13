fac35cd657597c975c6b1c668750ed4cb2b67d2d
a-zz <antonio@pareidolia.es>
2021-01-13 19:53:18 +0100

    Activity grid layout mockup.

--- src/app/act-grid/act-grid.component.ts
+++ src/app/act-grid/act-grid.component.ts
@@ -19,13 +19,14 @@ import { NavigationComponent }	from '../navigation/navigation.component';
 export class ActGridComponent implements OnInit {
 
 	myActList :Array<Activity> = []; 
+	gridData : Array<any> = [];
 	
 	constructor(private _http :HttpClient, private _nav: NavigationComponent) { 
 		
+		this._nav.setLocation('My activity', 'table_chart');
 		this.getActivity();
-		this._nav.setLocation('My activity', 'table_chart');		
 	}
-
+		
 	ngOnInit() :void {
 
 	}
@@ -70,6 +71,12 @@ export class ActGridComponent implements OnInit {
 									});
 								});	
 								this.myActList = myActList;
+								// TODO Test code
+								this.gridData = [
+									{ "activity": myActList[0].name, "precol": "", "moncol": "0:00", "tuecol": "0:00", "wedcol": "0:00", "thucol": "0:00", "fricol": "0:00", "satcol": "0:00", "suncol": "0:00", "postcol": "0:00" },
+									{ "activity": myActList[1].name, "precol": "", "moncol": "0:00", "tuecol": "0:00", "wedcol": "0:00", "thucol": "0:00", "fricol": "0:00", "satcol": "0:00", "suncol": "0:00", "postcol": "0:00" },
+									{ "activity": myActList[2].name, "precol": "", "moncol": "0:00", "tuecol": "0:00", "wedcol": "0:00", "thucol": "0:00", "fricol": "0:00", "satcol": "0:00", "suncol": "0:00", "postcol": "0:00" }
+								];
 								this._nav.showWait(false);
 							});
 					});
@@ -86,6 +93,10 @@ export class ActGridComponent implements OnInit {
 		});
 		*/
 	}
+	
+	// TODO Test code
+	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;	
 }
 
 export class Activity {
@@ -114,4 +125,25 @@ export class Activity {
 		} 
 	}
 }
+
+// TODO Test code
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
