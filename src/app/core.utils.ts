f7c02e2c12de2e7a1ffa7cc8c90bbcbaae7ee5c6
a-zz <antonio@pareidolia.es>
2021-01-18 16:47:43 +0100

    Code cleanup.

--- src/app/core.utils.ts
+++ src/app/core.utils.ts
@@ -4,8 +4,6 @@
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
 import { HttpClient, HttpHeaders } 				
 	from '@angular/common/http';
-import { Injectable } 				
-	from '@angular/core';
 
 import * as _ 						
 	from 'lodash';
@@ -199,97 +197,6 @@ export class History {
 					});
 				}
 			});
-		}
-	}
-}
-
-@Injectable({ providedIn: 'root' })
-export class RefChips {
-	
-	rccache :Array<{ "expiry": number, "data": any }>;
-	
-	constructor() {
-		
-		this.rccache = [];
-		console.log('Should be shown just once!')
-	}
-	
-	// TODO Add click event on chips and redirect accordingly (not so immediate if relatedTo) 
-	fillInAll(http :HttpClient) {
-		
-		let self = this;
-		let chips = document.querySelectorAll(".refchip");
-		_.forEach(chips, function(chip) {
-			let cached = self.getCached(chip.id);
-			if(cached)
-				self.fillIn(chip as HTMLElement, cached.data);
-			else {
-				let dbObjUrl = `/_data/${chip.id}`;
-				http.get(dbObjUrl, { "observe": "body", responseType: "json" }).subscribe((data :any) => {
-					// TODO Handle errors properly
-					if(!data.error) {
-						self.fillIn(chip as HTMLElement, data);
-						self.cacheIn(data);
-					}
-					else
-						self.fillIn(chip as HTMLElement, { "type": "error", "reason": `(${data.reason})` });
-				});
-			}
-		});
-	}
-	
-	getCached(_id :string) :{ "expiry": number, "data": any }|null {
-		
-		let cached = _.find(this.rccache, function(o) {
-			return o.data._id==_id;
-		});
-		if(cached) {
-			if(cached.expiry>=_.now())
-				return cached;
-			else {
-				_.remove(this.rccache, function(o) {
-					return o.data._id==_id;
-				});
-				return null;
-			}
-		}
-		else
-			return null;
-	}
-	
-	cacheIn(data :any) :void {
-		
-		_.remove(this.rccache, function(o) {
-			return o.data._id==data._id;
-		});
-		let expiry = _.now();
-		switch(data.type) {
-			case "Note":
-			case "ProjectTask":
-				expiry += 10 * 60 * 1000; // 10 minutes
-				break;
-			default:
-				expiry += 60 * 60 * 1000; // default: 1 hour 
-		}
-		this.rccache.push({ "expiry": expiry, "data": data })
-	}
-		
-	// FIXME Method contract missing
-	fillIn(chip :HTMLElement, data :any) :void {
-		
-		switch(data.type) {
-		case "Usr":
-			chip.innerHTML = data.email;
-			break;
-		case "Note":
-		case "ProjectTask":
-			chip.innerHTML = data.summary;
-			break;
-		case "error":
-			chip.innerHTML = data.reason;
-			break;
-		default:
-			chip.innerHTML = '???';
 		}
 	}
 }
